91a2ef7649a316937fb2e0452abf0abb2a3a9835
Ramon Mesquita <ramonmesquitamacedo2222@gmail.com>
2023-04-22 14:28:04 -0300

    Exercício concluído

---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Olá, humano");
+
+Console.WriteLine("Precione uma tecla para continuar...");
+
+Console.ReadKey();
+
+Console.WriteLine("Obrigado");
+Console.WriteLine("Tchau humano");
